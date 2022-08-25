321566d7f86a518734bae397b116f7b4048f9e85
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-25 15:15:07 +0300

    COMPLETED: ara - one to many

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  
+    #using macros
+    has_many :reviews #generates instance method, #reviews used to access data about reviews from game 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+    # #   a review belongs to a game
+    # def game
+    #     # self is the reviewing instance
+    #     Game.find(self.game_id)
+    # end
+
+    #using macros 
+    belongs_to :game #=> generates instance method, #game used to access data about game from review 
 end
